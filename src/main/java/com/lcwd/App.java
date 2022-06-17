d8b0d472f771ed302618d8023653417663ce1de1
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-06-18 02:06:34 +0530

    Added : with session object

--- src/main/java/com/lcwd/App.java
+++ src/main/java/com/lcwd/App.java
@@ -1,5 +1,11 @@
 package com.lcwd;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,48 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello This is Ajit..!!!" );
+        
+        System.out.println("Preparing to send the message");
+        String message = "Hello, Dear This message for security purpose";
+        String subject = "CodersArea : confimatio";
+        String to = "vibhuteajit752@gmail.com";
+        String from = "vibhuteajit3@gmail.com";
+        
+        sendEmail(message,subject,to,from);
+       
     }
+
+	private static void sendEmail(String message, String subject, String to, String from) {
+		
+		// Variable for gmail
+		String host ="smtp.gmail.com";
+		
+		Properties properties = System.getProperties();
+		System.out.println("PROPERTIES"  +properties);
+		
+		//setting important to properties object
+		
+		// host set
+		
+		properties.put("mail.smtp.host", host);
+		properties.put("mail.smtp.port", "465");
+		properties.put("mail.smtp.ssl.enable", "true");
+		properties.put("mail.smtp.auth", "true");
+		
+		// Step 1: to get the session object
+		Session session =Session.getInstance(properties, new Authenticator() {
+
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+			
+				return new PasswordAuthentication("vibhuteajit752@gmail.com", "9850962723");
+			}
+			
+		});
+		
+		
+		
+		
+		
+		}
 }
